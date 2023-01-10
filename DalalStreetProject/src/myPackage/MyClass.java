3c8b757fce7c21cf8fd77d4a0b71615d81a8fe5e
shilpa583 <122080574+shilpa583@users.noreply.github.com>
2023-01-10 12:50:38 +0530

    Basic Steps
    Hi  Team
    
    i have created framework structure go head  and clone  start with  your task know

---
+++ DalalStreetProject/src/myPackage/MyClass.java
@@ -0,0 +1,12 @@
+package myPackage;
+
+public class MyClass {
+	public static void main(String args[]) {
+	
+	System.out.println("Added Dependences");
+	System.out.println("Extend Report");
+	System.out.println("Log4");
+	System.out.println("Basic shift");
+
+}
+}
