48ba7d06644b681115ea58bff22c9ff4e38d5bc0
shilpa583 <122080574+shilpa583@users.noreply.github.com>
2023-01-10 17:36:20 +0530

    homepage task
    Hi
    
    I have completed homepage task please view it.

---
+++ DalalStreetProject/src/pagelayer/Homepage.java
@@ -0,0 +1,12 @@
+package pagelayer;
+
+public class Homepage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Home page task completed");
+
+	}
+
+}
